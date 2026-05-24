80d4a7f4c13c40f3b4ce2b7bbf9dc81714f0b566
sm1ckers <szmikgergo@gmail.com>
2026-05-24 18:00:17 +0200

    Teszt projekt hozzáadása 2 teszttel

---
+++ Temalabor.Tests/Test1.cs
@@ -0,0 +1,21 @@
+﻿namespace Temalabor.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+[TestClass]
+public class Test1
+{
+    [TestMethod]
+    public void Test_Pelda1()
+    {
+        
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public void Test_Pelda2()
+    {
+      
+        int a = 2;
+        int b = 3;
+        Assert.AreEqual(5, a + b);
+    }
+}
